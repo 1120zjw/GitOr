7ed7a7ad42207d6e60a9ad67aa274a867c4a2ebc
1120zjw <1727913368@qq.com>
2020-08-08 10:59:06 +0800

    user_2

--- src/Demo1.java
+++ src/Demo1.java
@@ -2,4 +2,7 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("第一个git仓库");
     }
+    public void me(){
+        System.out.println("youme");
+    }
 }
